4566af9ddb6da1de0b0635cbb708e9ef8a363ce5
kUm1l <kamil.gunia699@gmail.com>
2023-06-18 07:17:43 +0200

    Create aplikacjaApp.h

---
+++ src/frontRPG/testy CB/aplikacja/aplikacjaApp.h
@@ -0,0 +1,22 @@
+/***************************************************************
+ * Name:      aplikacjaApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2023-06-17
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef APLIKACJAAPP_H
+#define APLIKACJAAPP_H
+
+#include <wx/wx.h>
+#include "aplikacjaMain.h"
+
+class aplikacjaApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+#endif // APLIKACJAAPP_H
