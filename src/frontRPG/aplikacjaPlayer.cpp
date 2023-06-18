c95166e5570c6e30ea5fe7eeb791ef0e78e3278a
kUm1l <kamil.gunia699@gmail.com>
2023-06-18 07:17:19 +0200

    Create aplikacjaPlayer.cpp

---
+++ src/frontRPG/aplikacjaPlayer.cpp
@@ -0,0 +1,27 @@
+#include "aplikacjaPlayer.h"
+
+aplikacjaPlayerPanel::aplikacjaPlayerPanel(wxNotebook* parent, const wxString& playerName)
+    : wxPanel(parent, wxID_ANY)
+{
+    playerNotebook = new wxNotebook(this, wxID_ANY);
+
+    wxPanel* statsPanel = new wxPanel(playerNotebook, wxID_ANY);
+    playerNotebook->AddPage(statsPanel, "Stats");
+
+    wxPanel* equippedEQPanel = new wxPanel(playerNotebook, wxID_ANY);
+    playerNotebook->AddPage(equippedEQPanel, "Equipped EQ");
+
+    wxPanel* eqPanel = new wxPanel(playerNotebook, wxID_ANY);
+    playerNotebook->AddPage(eqPanel, "EQ");
+
+    wxPanel* statesPanel = new wxPanel(playerNotebook, wxID_ANY);
+    playerNotebook->AddPage(statesPanel, "States");
+
+    wxPanel* addPanel = new wxPanel(playerNotebook, wxID_ANY);
+    playerNotebook->AddPage(addPanel, "Add");
+
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(playerNotebook, 1, wxEXPAND);
+    SetSizer(sizer);
+}
+
