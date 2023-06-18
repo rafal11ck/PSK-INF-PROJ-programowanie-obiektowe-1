fc0c5d06b645f1d03905b53bd3adc39536c464ee
kUm1l <kamil.gunia699@gmail.com>
2023-06-18 12:51:56 +0200

    Update appPlayer.hpp

--- src/frontRPG/appPlayer.hpp
+++ src/frontRPG/appPlayer.hpp
@@ -5,6 +5,16 @@
 #include <wx/notebook.h>
 #include <wx/button.h>
 #include <wx/textctrl.h>
+#include <wx/listctrl.h>
+
+enum
+{
+    ID_CHAR_ADD_STAT = wxID_HIGHEST + 1,
+    ID_CHAR_ADD_EQUIPMENT,
+    ID_CHAR_ADD_STATE,
+    ID_CHAR_EQUIP_EQUIPMENT,
+    ID_CHAR_UNEQUIP_EQUIPMENT
+};
 
 class appPlayerPanel : public wxPanel
 {
@@ -13,6 +23,42 @@ public:
 
 private:
     wxNotebook* playerNotebook;
+
+    wxPanel* statsPanel;
+    wxPanel* equippedEQPanel;
+    wxPanel* eqPanel;
+    wxPanel* statesPanel;
+    wxPanel* addPanel;
+
+    wxBoxSizer* statsSizer;
+    wxBoxSizer* equippedEQSizer;
+    wxBoxSizer* eqSizer;
+    wxBoxSizer* statesSizer;
+    wxBoxSizer* addPanelSizer;
+    wxBoxSizer* mainSizer;
+
+    wxListCtrl* statsListCtrl;
+    wxListCtrl* equippedEQListCtrl;
+    wxListCtrl* eqListCtrl;
+    wxListCtrl* statesListCtrl;
+
+    wxButton* addStatButton;
+    wxButton* addEquipmentButton;
+    wxButton* addStateButton;
+    wxButton* equipEquipmentButton;
+    wxButton* unequipEquipmentButton;
+
+    void OnAddStat(wxCommandEvent& event);
+    void OnAddEquipment(wxCommandEvent& event);
+    void OnAddState(wxCommandEvent& event);
+    void OnEquipEquipment(wxCommandEvent& event);
+    void OnUnequipEquipment(wxCommandEvent& event);
+    void UpdateStatsListCtrl();
+    void UpdateEquippedEQListCtrl();
+    void UpdateEQListCtrl();
+    void UpdateStatesListCtrl();
+
+    wxDECLARE_EVENT_TABLE();
 };
 
 #endif // APPPLAYER_H
